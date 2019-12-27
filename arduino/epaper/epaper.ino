68a31fe280d82bad2486a77fb612c2428c4b3e18
theapi <petermclarke@gmail.com>
2019-12-27 22:29:26 +0000

    Basic info on the screen

--- arduino/epaper/epaper.ino
+++ arduino/epaper/epaper.ino
@@ -23,10 +23,21 @@ const byte DEBUG_LED = 16;
 PAYLOAD_sensor_t payload = {};
 
 GxEPD2_BW<GxEPD2_290, GxEPD2_290::HEIGHT> display(GxEPD2_290(/*CS=D8*/ 5, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
+// Display is 296x128
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
+
+  display.init(115200);
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+
+  // first update should be full refresh
+  //display.clearScreen();
+  showBoot(display);
+  display.powerOff();
 
   WiFi.begin(ssid, password);
   Serial.print("\nConnecting to "); Serial.println(ssid);
@@ -46,31 +57,63 @@ void setup() {
   pinMode(DEBUG_LED, OUTPUT);
   digitalWrite(DEBUG_LED, HIGH);  // LOW = ON
 
-  Serial.println("Display setup");
-  delay(100);
-  display.init(115200);
-  // first update should be full refresh
-  helloWorld(display);
-  delay(500);
-  // partial refresh mode can be used to full screen,
-  // effective if display panel hasFastPartialUpdate
-  helloFullScreenPartialMode(display);
-  delay(1000);
-  helloArduino(display);
-  delay(1000);
-  helloEpaper(display);
-//  delay(1000);
-//  //helloValue(display, 123.9, 1);
-//  //delay(1000);
-//  showFont(display, "FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
-
+  showConnected(display);
   display.powerOff();
 
-  Serial.println("Display setup done");
+
+}
+
+void showBoot(GxEPD2_GFX& display) {
+  const char text[] = "WSN Monitor";
+  display.setFullWindow();
+  int16_t tbx, tby; uint16_t tbw, tbh; // boundary box window
+  display.getTextBounds(text, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center bounding box by transposition of origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(text);
+  }
+  while (display.nextPage());
+}
+
+void showConnected(GxEPD2_GFX& display) {
+  char buf[64];
+  uint16_t x = 0;
+  uint16_t y = 20;
+
+  display.setPartialWindow(0, 0, display.width(), 40);
+  sprintf(buf, "IP %s,\nUDP port %d", ipMulti.toString().c_str(), portMulti);
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(buf);
+  }
+  while (display.nextPage());
+}
+
+void showPayload(GxEPD2_GFX& display) {
+  char buf[64];
+  uint16_t x = 0;
+  uint16_t y = 120;
+
+  display.setPartialWindow(0, 100, display.width(), 24);
+  sprintf(buf, "Msg %d", payload.message_id);
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(buf);
+  }
+  while (display.nextPage());
 }
 
 void loop() {
-
+  static uint8_t msg_id = 0;
   // Check for udp data. 
   int packetSize = Udp.parsePacket();
   if (packetSize) {
@@ -81,8 +124,14 @@ void loop() {
       incomingPacket[len] = 0;
     }
     PAYLOAD_unserialize(&payload, (uint8_t*) incomingPacket);
-    Serial.print(payload.message_id); Serial.print(", ");
-    Serial.println();
+    if (payload.message_id != msg_id) {
+      msg_id = payload.message_id;
+      showPayload(display);
+      display.powerOff();
+      Serial.print(payload.message_id); Serial.print(", ");
+      Serial.println();
+    }
+    
   }
 
 }
